69ed7fd22c2703ca2e0843f289719ae1eeee9219
Marcos Junior <marcossouzacruzjunior@gmail.com>
2020-01-03 14:49:24 -0300

    Curso De JavaAcript e ECMAScript para iniciantes

---
+++ CursoJS/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log('Você é  Brasileiro Infelizmente!')
+}else{
+    console.log('Você é um Estrangeiro graças a Deus')
+}
